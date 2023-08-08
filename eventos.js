5e33d9b0da1f42d4f5d2c0ef02e11062c447cafd
Natasha Miquele <natymiquele08@gmail.com>
2023-08-08 16:22:03 -0300

    seguir probando

--- eventos.js
+++ eventos.js
@@ -1,8 +1,9 @@
 let div = document.getElementById("boton")
-let boton = document.getElementsByClassName("btn");
+
 
 
 div.addEventListener("click", function() {
   alert("Hola soy un div!");
 });
+
 
